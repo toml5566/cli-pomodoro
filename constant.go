0a163f7e6052075612fee30240aa2b744fffbfe3
tom <tomleung5566@gmail.com>
2025-08-03 01:39:06 +0800

    Add constants and ASCII art for timer application

---
+++ constant.go
@@ -0,0 +1,127 @@
+package main
+
+// Timer states
+type SessionType int
+
+const (
+	Focus SessionType = iota
+	ShortBreak
+	LongBreak
+)
+
+// Session durations (in seconds for easier testing, change to minutes * 60 for production)
+const (
+	FocusDuration      = 25 * 60 // 25 minutes
+	ShortBreakDuration = 5 * 60  // 5 minutes
+	LongBreakDuration  = 15 * 60 // 15 minutes
+)
+
+// background color codes
+const (
+	FocusBgColor      = "#1a1a2e" // Dark blue
+	ShortBreakBgColor = "#16537e" // Green-blue
+	LongBreakBgColor  = "#0f3460" // Darker blue-green
+)
+
+// ASCII art for large numbers
+var asciiNumbers = map[string][]string{
+	"0": {
+		" ███████ ",
+		"██     ██",
+		"██     ██",
+		"██     ██",
+		"██     ██",
+		"██     ██",
+		" ███████ ",
+	},
+	"1": {
+		"    ██   ",
+		"  ████   ",
+		"    ██   ",
+		"    ██   ",
+		"    ██   ",
+		"    ██   ",
+		"  ██████ ",
+	},
+	"2": {
+		" ███████ ",
+		"██     ██",
+		"       ██",
+		" ███████ ",
+		"██       ",
+		"██       ",
+		"█████████",
+	},
+	"3": {
+		" ███████ ",
+		"██     ██",
+		"       ██",
+		" ███████ ",
+		"       ██",
+		"██     ██",
+		" ███████ ",
+	},
+	"4": {
+		"██     ██",
+		"██     ██",
+		"██     ██",
+		"█████████",
+		"       ██",
+		"       ██",
+		"       ██",
+	},
+	"5": {
+		"█████████",
+		"██       ",
+		"██       ",
+		"████████ ",
+		"       ██",
+		"██     ██",
+		" ███████ ",
+	},
+	"6": {
+		" ███████ ",
+		"██     ██",
+		"██       ",
+		"████████ ",
+		"██     ██",
+		"██     ██",
+		" ███████ ",
+	},
+	"7": {
+		"█████████",
+		"       ██",
+		"      ██ ",
+		"     ██  ",
+		"    ██   ",
+		"   ██    ",
+		"  ██     ",
+	},
+	"8": {
+		" ███████ ",
+		"██     ██",
+		"██     ██",
+		" ███████ ",
+		"██     ██",
+		"██     ██",
+		" ███████ ",
+	},
+	"9": {
+		" ███████ ",
+		"██     ██",
+		"██     ██",
+		" ████████",
+		"       ██",
+		"██     ██",
+		" ███████ ",
+	},
+	":": {
+		"         ",
+		"   ███   ",
+		"   ███   ",
+		"         ",
+		"   ███   ",
+		"   ███   ",
+		"         ",
+	},
+}
